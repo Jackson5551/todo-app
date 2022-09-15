a0688f442ff0d81e1f257a8646cb4b723a96e0e6
Jackson Bingham <45270054+Jackson5551@users.noreply.github.com>
2022-09-15 16:46:24 -0600

    add and delete list functionality working

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ const addListInput = document.querySelector('#addListInput')
 const allListsUL = document.querySelector('#allLists')
 const viewToDoListDiv = document.querySelector('#viewToDoList')
 const addListForm = document.querySelector('#addListForm')
-const deleteListButton = document.querySelector('deleteListButton')
+let deleteListButton = document.querySelector('#deleteListButton')
 
 
 const LOCAL_STORAGE_KEY = 'todoApp.listKey'
-const listsFromLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+const LOCAL_STORAGE_KEY_IS_SELECTED_ID_KEY = 'todoApp.isSelected'
+let listsFromLocalStorage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []
+let selectedListID = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_IS_SELECTED_ID_KEY)) || []
 
 class buildObjects {
     static createList(name) {
@@ -17,18 +19,17 @@ class buildObjects {
     }
 }
 
-
 class storageHandler {
     static getLists() {
         let lists;
         if (localStorage.getItem(LOCAL_STORAGE_KEY) === null) {
             lists = []
-            console.log(lists)
+            // console.log(lists)
         } else {
             lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-            console.log(lists)
+            // console.log(lists)
         }
-        console.log(lists)
+        // console.log(lists)
         return lists;
     }
 
@@ -39,40 +40,72 @@ class storageHandler {
     }
 
     static removeList(date) {
+        console.log("Clicked!" + +date)
         let lists = storageHandler.getLists()
-        lists.forEach((list, index) => {
-            if (list.date === date) {
-                lists.splice(index, 1);
+        lists.forEach((list, i) => {
+            if (+list.id === +date) {
+                listsFromLocalStorage.splice(i, 1);
+                console.log("SUCCESS " + i)
+            } else{
+                console.log("FAILED " + i )
             }
+            i++
         })
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(lists))
+        // listsFromLocalStorage.push(lists)
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(listsFromLocalStorage))
+        saveAndRender()
     }
 }
 
 class UIHandler {
-    static displayLists() {
-        const lists = storageHandler.getLists();
-        clearElement(allListsUL)
-        lists.forEach((list) => UIHandler.addListToView(list));
-    }
+    // static displayLists() {
+    //     // const lists = storageHandler.getLists();
+    //     clearElement(allListsUL)
+    //     listsFromLocalStorage.forEach((list) => UIHandler.addListToView(list));
+    // }
     static addListToView(list) {
         const view = allListsUL;
         // clearElement(view)
-        view.innerHTML = `
-        <li class="list-group-item d-flex justify-content-between align-items-start">
+        let listToAdd = `
+        <li id="${list.id}" class="list-group-item list-group-item-action d-flex justify-content-between align-items-start">
             <div class="ms-2 me-auto">
                 <div class="fw-bold">${list.name}</div>
-                    ${list.desc}
+                    This is a test card
             </div>
             <span class="d-flex flex-column justify-content-between">
-                <span class="badge bg-danger rounded-pill mb-1" id="deleteButton"><i class="bi bi-trash-fill"></i></span>
-                <span class="badge bg-warning rounded-pill" id="editButton"><i class="bi bi-pencil-fill"></i></span>
+
+                <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                    <button type="button" class="btn btn-danger" id="deleteListButton" onclick="storageHandler.removeList(${list.id})"><i class="bi bi-trash-fill" id="deleteListButton"></i></button>
+                    <button type="button" class="btn btn-light"><i class="bi bi-pencil-fill"></i></button>
+                </div>
             </span>
         </li>
-        ` + view.innerHTML
-        console.log(list)
+        `
+        view.innerHTML = listToAdd + view.innerHTML
+        // console.log(list)
     }
-    static deleteList() {
+    static updateToActive(list) {
+        const view = allListsUL;
+        // clearElement(view)
+        let listToAdd = `
+        <li id="${list.id}" class="list-group-item list-group-item-action active d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+                <div class="fw-bold">${list.name}</div>
+                    This is a test card
+            </div>
+            <span class="d-flex flex-column justify-content-between">
+
+                <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                    <button type="button" class="btn btn-danger" id="deleteListButton" onclick="storageHandler.removeList(${list.id})"><i class="bi bi-trash-fill" id="deleteListButton"></i></button>
+                    <button type="button" class="btn btn-light"><i class="bi bi-pencil-fill"></i></button>
+                </div>
+            </span>
+        </li>
+        `
+        view.innerHTML = listToAdd + view.innerHTML
+        // console.log(list)
+    }
+    static deleteList(listID) {
 
     }
 }
@@ -82,32 +115,59 @@ class UIHandler {
 addListForm.addEventListener('submit', e => {
     e.preventDefault()
     const listName = addListInput.value
-    const newList = buildObjects.createList(listName)
-    addListInput.value = null
-    // listsFromLocalStorage = storageHandler.getLists()
-    listsFromLocalStorage.push(newList)
-    console.log(newList)
-    saveAndRender()
+    if (listName == null || listName == '') {
+        return
+    } else {
+        const newList = buildObjects.createList(listName)
+        addListInput.value = null
+        listsFromLocalStorage.push(newList)
+        // console.log(newList)
+        selectedListID = newList.id
+        saveAndRender()
+    }
 })
 
+allListsUL.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListID = e.target.id
+        saveAndRender()
+    }
+})
+
+// deleteListButton.addEventListener('click', e => {
+    
+//     console.log('Im here')
+//     if (e.target.tagName.toLowerCase() === 'li') {
+//         listToRemove = e.target.id
+//         storageHandler.removeList(listToRemove)
+//         // listsFromLocalStorage = listsFromLocalStorage.filter(list => list.id !== selectedListID)
+//         selectedListID = null
+//     }
+// })
+
 function render() {
-    clearElement(viewToDoListDiv)
-    UIHandler.displayLists()
-
-    const selectedList = listsFromLocalStorage.find(list => list.id === selectedListID)
-    // if(selectedListID == null){
-    //     return
-    // } else {
-    //     selectedList.forEach(element => {
-
-    //     });
-    // }
+    clearElement(allListsUL)
+    renderLists()
 }
-function clearElement(element){
+function renderLists() {
+    listsFromLocalStorage.forEach(list => {
+        const selectedList = listsFromLocalStorage.find(list => list.id === selectedListID)
+        // console.log(selectedList)
+        if (selectedList == null) {
+            UIHandler.addListToView(list)
+        } else if (list.id == +selectedListID) {
+            UIHandler.updateToActive(list)
+        } else {
+            UIHandler.addListToView(list)
+        }
+    });
+}
+function clearElement(element) {
     element.innerHTML = ''
 }
 function save() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(listsFromLocalStorage))
+    localStorage.setItem(LOCAL_STORAGE_KEY_IS_SELECTED_ID_KEY, JSON.stringify(selectedListID))
 }
 
 function saveAndRender() {
@@ -115,4 +175,4 @@ function saveAndRender() {
     render()
 }
 
-render()
+document.addEventListener('DOMContentLoaded', renderLists())
